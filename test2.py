ed8ee6d0f6d251e9feaf0cfb8e5761e5e5f58012
choi's_Chicken <168711938+choimj1717@users.noreply.github.com>
2025-08-12 20:49:21 +0900

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,13 @@
+# cam_yolo.py
+from ultralytics import YOLO
+
+# 모델 로드
+model = YOLO("medel_1.pt")
+
+# 웹캠에서 실시간 추론
+model.predict(
+    source=1,      # 0은 기본 웹캠
+    conf=0.5,      # confidence threshold (0~1)
+    show=True,     # 추론 결과 창에 표시
+    save=False     # 저장하지 않음, True로 바꾸면 runs/predict 폴더에 저장
+)
